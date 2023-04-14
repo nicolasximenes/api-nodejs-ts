e92b82e8a90eacbbe99fc084a91bec23802afd67
Nicolas Ximenes <nicolasximenes2@gmail.com>
2023-04-13 22:33:41 -0300

    fix: verification env for run migrations

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 import { server } from './server/Server'
+import { Knex } from './server/database/knex'
 
-server.listen(process.env.PORT || 3333, () => {
-  console.log(`App rodando na porta ${process.env.PORT || 3333}`)
-})
+const startServer = () => {
+  server.listen(process.env.PORT || 3333, () => {
+    console.log(`App rodando na porta ${process.env.PORT || 3333}`)
+  })
+}
+
+if (process.env.IS_LOCALHOST !== 'true') {
+  Knex.migrate.latest().then(() => {
+    startServer()
+  }).catch(console.log)
+} else {
+  startServer()
+}
