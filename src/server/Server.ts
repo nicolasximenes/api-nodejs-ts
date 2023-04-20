ca8370fef68c4ccc3d7af182cc568dc5a432f500
Nicolas Ximenes <nicolasximenes2@gmail.com>
2023-04-20 15:06:44 -0300

    fix: cors support added

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,10 +1,14 @@
 import express from 'express'
+import cors from 'cors'
 import { router } from './routes'
 import 'dotenv/config'
 import './shared/services/TranslationsYup'
 
 const server = express()
 
+server.use(cors({
+  origin: process.env.ENABLE_CORS?.split(';') || [],
+}))
 server.use(express.json())
 server.use(router)
 
